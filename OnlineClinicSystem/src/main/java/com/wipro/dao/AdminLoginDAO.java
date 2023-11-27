9de4f0c54104dafa087ced08f0d05c94486c1093
BadicoleVikasReddy <152059730+BadicoleVikasReddy@users.noreply.github.com>
2023-11-27 12:01:23 +0530

    Delete OnlineClinicSystem directory

--- OnlineClinicSystem/src/main/java/com/wipro/dao/AdminLoginDAO.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.wipro.POJO.AdminLoginPOJO;
-
-public interface AdminLoginDAO extends CrudRepository<AdminLoginPOJO, String> {
-
-}
